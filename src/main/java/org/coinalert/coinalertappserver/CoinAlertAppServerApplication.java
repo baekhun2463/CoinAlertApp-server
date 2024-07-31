4e83f7f4f904eee0465191b76203369c485e3598
baekhun <baekhun2463@gmail.com>
2024-07-31 19:46:05 +0900

    first commit

---
+++ src/main/java/org/coinalert/coinalertappserver/CoinAlertAppServerApplication.java
@@ -0,0 +1,13 @@
+package org.coinalert.coinalertappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinAlertAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinAlertAppServerApplication.class, args);
+    }
+
+}
